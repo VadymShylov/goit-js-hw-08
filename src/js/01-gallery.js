7855c1e4fd1d815df0efa682f83653400e5aae9a
VadymShylov <shylovvadym@gmail.com>
2022-08-02 16:27:17 +0100

    bugs fixed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 // Add imports above this line
 import SimpleLightbox from "simplelightbox";
 
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 
 import "simplelightbox/dist/simple-lightbox.min.css";
 
